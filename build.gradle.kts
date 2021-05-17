664c58531606c337ce99a218e8960cad61a0ee8c
joeyt <joeyt@slalom.com>
2021-05-16 20:00:42 -0700

    Workaround logback.xml issue 3
    Disable gcp-starter-logging and remove logback-spring.xml to get unblocked.

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("com.google.cloud:spring-cloud-gcp-starter")
-	implementation("com.google.cloud:spring-cloud-gcp-starter-logging")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
