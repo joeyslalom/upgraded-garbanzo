f8e7b1178599484c91bb437c5a52efe03a7ffa9e
joeyt <joeyt@slalom.com>
2021-05-23 14:01:53 -0700

    Cloud Run 2
    Try using a @TypeHint

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,7 +27,6 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("com.google.cloud:spring-cloud-gcp-starter")
-	implementation("com.google.cloud:google-cloud-graalvm-support:0.4.0")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
---
+++ src/main/kotlin/joeyslalom/upgradedgarbanzo/GcpNativeHints.kt
@@ -0,0 +1,10 @@
+package joeyslalom.upgradedgarbanzo
+
+import com.google.cloud.spring.autoconfigure.datastore.GcpDatastoreEmulatorAutoConfiguration
+import org.springframework.context.annotation.Configuration
+import org.springframework.nativex.hint.TypeHint
+
+
+@TypeHint(types = [GcpDatastoreEmulatorAutoConfiguration::class])
+@Configuration
+class GcpDatastoreEmulatorAutoConfigurationHint
