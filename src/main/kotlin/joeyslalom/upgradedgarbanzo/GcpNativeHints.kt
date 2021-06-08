27b906ba3ecf18eaeead8dd3c9114daa68347fa4
joeyt <joeyt@slalom.com>
2021-06-07 17:11:33 -0700

    Cloud SQL 3
    Bean instantiation via factory method failed; nested exception is org.springframework.beans.BeanInstantiationException: Failed to instantiate [com.zaxxer.hikari.HikariDataSource]: Factory method 'dataSource' threw exception; nested exception is org.springframework.boot.autoconfigure.jdbc.DataSourceProperties$DataSourceBeanCreationException: Failed to determine a suitable driver class

--- src/main/kotlin/joeyslalom/upgradedgarbanzo/GcpNativeHints.kt
+++ src/main/kotlin/joeyslalom/upgradedgarbanzo/GcpNativeHints.kt
@@ -2,10 +2,14 @@ package joeyslalom.upgradedgarbanzo
 
 import com.google.cloud.spring.autoconfigure.datastore.GcpDatastoreEmulatorAutoConfiguration
 import com.google.cloud.spring.autoconfigure.sql.CloudSqlEnvironmentPostProcessor
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.nativex.hint.TypeHint
 
 
 @TypeHint(types = [CloudSqlEnvironmentPostProcessor::class, GcpDatastoreEmulatorAutoConfiguration::class])
 @Configuration
-class GcpAutoConfigurationHint
+class GcpAutoConfigurationHint {
+    @Bean
+    fun cloudSql() = CloudSqlEnvironmentPostProcessor()
+}
