12856fda4ee36f64b938de75bb8695613fc75eec
joeyt <joeyt@slalom.com>
2021-06-12 18:55:21 -0700

    Cloud SQL 23
    java.lang.RuntimeException: [slalom-2020-293920:us-central1:upgraded-garbanzo] Failed to update metadata for Cloud SQL instance.

--- src/main/kotlin/joeyslalom/upgradedgarbanzo/GcpNativeHints.kt
+++ src/main/kotlin/joeyslalom/upgradedgarbanzo/GcpNativeHints.kt
@@ -1,18 +1,14 @@
 package joeyslalom.upgradedgarbanzo
 
 import com.google.api.client.googleapis.json.GoogleJsonError
-import com.google.api.client.googleapis.services.json.AbstractGoogleJsonClient
-import com.google.api.client.googleapis.services.json.AbstractGoogleJsonClientRequest
 import com.google.api.client.json.GenericJson
 import com.google.api.client.util.ClassInfo
 import com.google.api.client.util.Data
 import com.google.api.client.util.GenericData
 import com.google.api.services.sqladmin.SQLAdmin
+import com.google.api.services.sqladmin.SQLAdminRequest
 import com.google.api.services.sqladmin.model.DatabaseInstance
 import com.google.api.services.sqladmin.model.Operation
-import com.google.auth.oauth2.ServiceAccountCredentials
-import com.google.auth.oauth2.ServiceAccountJwtAccessCredentials
-import com.google.cloud.spring.autoconfigure.core.GcpProperties
 import com.google.cloud.spring.autoconfigure.datastore.GcpDatastoreEmulatorAutoConfiguration
 import com.google.cloud.spring.autoconfigure.sql.CloudSqlEnvironmentPostProcessor
 import com.google.cloud.spring.autoconfigure.sql.GcpCloudSqlProperties
@@ -43,7 +39,8 @@ class GcpCloudSqlHint
 @TypeHint(
     types = [GenericData::class, GoogleJsonError.ErrorInfo::class, GoogleJsonError::class,
         GenericJson::class, SQLAdmin::class, SQLAdmin.Operations.Get::class,
-    Data::class, ClassInfo::class, Operation::class, DatabaseInstance::class],
+        Data::class, ClassInfo::class, Operation::class, DatabaseInstance::class, SQLAdminRequest::class,
+        SQLAdmin.Instances.Get::class],
     typeNames = ["com.google.api.client.util.DataMap"]
 )
 @Configuration
