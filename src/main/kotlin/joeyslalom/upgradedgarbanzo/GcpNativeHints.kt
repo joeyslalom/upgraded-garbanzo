b6cc4de5efd54317c3c5c6ce75c00ddb9a5ec388
joeyt <joeyt@slalom.com>
2021-06-12 21:44:59 -0700

    Cloud SQL 27
    java.lang.InstantiationException: Type `com.google.api.services.sqladmin.model.IpConfiguration` can not be instantiated reflectively as it does not have a no-parameter constructor or the no-parameter constructor has not been added explicitly to the native image.
    
    Failed to create ephemeral certificate for the Cloud SQL instance. at com.google.cloud.sql.core.CloudSqlInstance.addExceptionContext(CloudSqlInstance.java:481) at com.google.cloud.sql.core.CloudSqlInstance.fetchEphemeralCertificate(CloudSqlInstance.java:341)

--- src/main/kotlin/joeyslalom/upgradedgarbanzo/GcpNativeHints.kt
+++ src/main/kotlin/joeyslalom/upgradedgarbanzo/GcpNativeHints.kt
@@ -7,7 +7,14 @@ import com.google.api.client.util.Data
 import com.google.api.client.util.GenericData
 import com.google.api.services.sqladmin.SQLAdmin
 import com.google.api.services.sqladmin.SQLAdminRequest
-import com.google.api.services.sqladmin.model.*
+import com.google.api.services.sqladmin.model.AclEntry
+import com.google.api.services.sqladmin.model.DatabaseFlags
+import com.google.api.services.sqladmin.model.DatabaseInstance
+import com.google.api.services.sqladmin.model.IpConfiguration
+import com.google.api.services.sqladmin.model.Operation
+import com.google.api.services.sqladmin.model.Settings
+import com.google.api.services.sqladmin.model.SslCert
+import com.google.api.services.sqladmin.model.SslCertsCreateEphemeralRequest
 import com.google.cloud.spring.autoconfigure.datastore.GcpDatastoreEmulatorAutoConfiguration
 import com.google.cloud.spring.autoconfigure.sql.CloudSqlEnvironmentPostProcessor
 import com.google.cloud.spring.autoconfigure.sql.GcpCloudSqlProperties
@@ -38,7 +45,8 @@ class GcpCloudSqlHint
 @TypeHint(
     types = [SQLAdmin::class, SQLAdmin.Operations.Get::class, SQLAdmin.Instances.Get::class,
         Operation::class, DatabaseInstance::class, SQLAdminRequest::class,
-        DatabaseFlags::class, Settings::class, SslCert::class, AclEntry::class]
+        DatabaseFlags::class, Settings::class, SslCert::class,
+        AclEntry::class, IpConfiguration::class, SslCertsCreateEphemeralRequest::class]
 )
 @Configuration
 class GcpSqlAdminHint
