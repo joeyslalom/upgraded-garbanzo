5f0babb536030ceed0a6528eb2f7e923d9f05037
joeyt <joeyt@slalom.com>
2021-07-02 15:09:50 -0700

    Gradle 6.9 7
    Add old repo to demonstrate using jdbcTemplate.
    Current error:
    Caused by: java.lang.IllegalStateException: Class proxy missing at runtime, hint required at build time: @AotProxyHint(targetClass=joeyslalom.upgradedgarbanzo.UserJdbcRepo.class, proxyFeatures = ProxyBits.IS_STATIC)
    
    Try same functionality again, but eliminate the class.

--- src/main/kotlin/joeyslalom/upgradedgarbanzo/GcpRestController.kt
+++ src/main/kotlin/joeyslalom/upgradedgarbanzo/GcpRestController.kt
@@ -14,14 +14,14 @@ import org.springframework.web.bind.annotation.RestController
 import java.sql.ResultSet
 
 @RestController
-class UserRestController(private val repo: UserRepo, private val jdbcRepo: UserJdbcRepo) {
+class UserRestController(private val repo: UserRepo, private val jdbcTemplate: JdbcTemplate) {
     private val log = LoggerFactory.getLogger(UserRestController::class.java)
 
     @GetMapping("/users")
     fun users(): List<User> = repo.findAll().toList()
 
     @GetMapping("/jdbcUsers")
-    fun jdbcUsers(): List<User> = jdbcRepo.findAll()
+    fun jdbcUsers(): List<User> = jdbcTemplate.query("SELECT email, first_name, last_name FROM users", userMapper)
 }
 
 @Table("users")
@@ -30,17 +30,17 @@ data class User(val email: String, val firstName: String, val lastName: String)
 @Repository
 interface UserRepo : CrudRepository<User, String>
 
-@Repository
-class UserJdbcRepo(private val jdbcTemplate: JdbcTemplate) {
-
-    val userMapper = RowMapper<User> { rs: ResultSet, _ ->
-        User(rs.getString("email"), rs.getString("first_name"), rs.getString("last_name"))
-    }
-
-    fun findAll(): List<User> {
-        return jdbcTemplate.query("SELECT email, first_name, last_name FROM users", userMapper)
-    }
+val userMapper = RowMapper<User> { rs: ResultSet, _ ->
+    User(rs.getString("email"), rs.getString("first_name"), rs.getString("last_name"))
 }
+
+//@Repository
+//class UserJdbcRepo(private val jdbcTemplate: JdbcTemplate) {
+//
+//    fun findAll(): List<User> {
+//        return jdbcTemplate.query("SELECT email, first_name, last_name FROM users", userMapper)
+//    }
+//}
 
 @Component
 class LogVersion(private val gitProperties: GitProperties) : InitializingBean {
