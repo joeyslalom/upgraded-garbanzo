9612c7cda125c9b9ebfc02b6cd182f61beb162fe
joeyt <joeyt@slalom.com>
2021-07-06 15:42:07 -0700

    @AotProxyHint 4
    It's official, @Repository is to blame.  Using @Component is a workaround.  Using @Repository throws:
    
    Caused by: java.lang.IllegalStateException: Class proxy missing at runtime, hint required at build time: @AotProxyHint(targetClass=joeyslalom.upgradedgarbanzo.UserJdbcRepo.class, proxyFeatures = ProxyBits.IS_STATIC)
    
    Also, adding the @AotProxyHint recommended does not resolve the issue.

--- src/main/kotlin/joeyslalom/upgradedgarbanzo/GcpRestController.kt
+++ src/main/kotlin/joeyslalom/upgradedgarbanzo/GcpRestController.kt
@@ -30,8 +30,8 @@ data class User(val email: String, val firstName: String, val lastName: String)
 @Repository
 interface UserRepo : CrudRepository<User, String>
 
-//@Component
-@Repository
+@Component
+//@Repository XXX Using @Repository instead of @Component causes a runtime failure
 class UserJdbcRepo(private val jdbcTemplate: JdbcTemplate) {
 
     val userMapper = RowMapper<User> { rs: ResultSet, _ ->
